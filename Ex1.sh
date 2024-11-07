4aabb7f543ad3aedf69d62070080c66c13e4c27d
Leonardo Pieripolli <leonardo.pieripolli@studenti.unipd.it>
2024-11-07 16:12:39 +0100

    [solutions] almost everything

--- Ex1.sh
+++ Ex1.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
 cd $HOME
-mkdir -p students
-if [ ! -f "./students/LCP_22-23_students.csv" ] 
+rm students/*
+rm -d students/
+mkdir -p students #makes dir if not existent
+if [ ! -f "./students/LCP_22-23_students.csv" ] #if not already present 
 then
-    wget -v --tries=1 https://www.dropbox.com/s/867rtx3az6e9gm8/LCP_22-23_students.csv --directory-prefix="./students"
+    wget -v --tries=1 https://www.dropbox.com/s/867rtx3az6e9gm8/LCP_22-23_students.csv --directory-prefix="./students" #import file in ./students
 fi
 cd students
-touch LCP_22-23_PoD_students.csv
+touch LCP_22-23_PoD_students.csv #create if not existing
 touch LCP_22-23_Physics_students.csv
-grep "PoD" LCP_22-23_students.csv > LCP_22-23_PoD_students.csv
+grep "PoD" LCP_22-23_students.csv > LCP_22-23_PoD_students.csv #copy only lines including PoD
 grep "Physics" LCP_22-23_students.csv > LCP_22-23_Physics_students.csv
 max=0
 max_L='A'
 for i in {A..Z}
-    do
-    #echo $i
-    j=`grep -c -e "^$i" -v -e "^Family" LCP_22-23_Physics_students.csv ` # | awk 'NR>1{print $1}'
+do
+    j=`grep -v -e  "^Family" LCP_22-23_students.csv | grep -c "^$i" LCP_22-23_students.csv` # first gives lines without Family Name, second counts starting with A
     echo "$i : $j"
-    if [ j>max ]
+    if [ $j -gt $max ] #compare values, not string
     then
         max=$j
         max_L=$i
     fi
-    done
+done
 echo "Max surname starting letter $max_L with $max entries"
-lines=`grep -c LCP_22-23_Physics_students.csvcat `
-i = 2
-for [ i<=lines ]
-    do
-    g=(i-1)%18
-    touch "Group$g.csv" 
-    awk 'NR==i{print}' LCP_22-23_students.csv > "Group$g.csv"
-    i++
-    done
+lines=`grep '' -c LCP_22-23_students.csv`
+i=2
+while [ $i -le $lines ]
+do
+    let g=($i-1)%18 #real computation for variables needs let or (( ))
+    file="Group$g.csv"
+    touch $file
+    awk NR==$i LCP_22-23_students.csv >> $file #selected line with awk
+    let i+=1
+done
+#spacing is always wrong in bash, unless is necessary
---
+++ Ex2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+grep -v '^#' data.csv | sed -e 's/,//g' > data.txt
+even=0
+for i in `cat data.txt`
+do
+    if [ $i%2 ] #%2 gives 0 if even, that correspond to true in bash
+    then
+        let even+=1
+    fi
+done
+echo "even numbers = $even"
+m=0
+l=0
+sigma=$( echo 'scale=6;100*sqrt(3)/2.0' | bc)
+FILE="data.txt"
+lines=`grep '' -c $FILE`
+i=1
+while [ $i -le $lines ]
+do
+    line=`awk NR==$i $FILE`
+    IFS=' ' read -r X Y Z x y z <<< "$line"
+    d=$( echo "scale=6;sqrt($X*$X+$Y*$Y+$Z*$Z)" | bc)
+    #echo $d
+    if [ `echo "$d < $sigma" | bc` -eq 0 ]
+    then
+        let m++
+    else
+        let l++
+    fi
+    let i++
+done
+echo "there are $m of distance grater than $sigma"
+echo "there are $l of distance smaller than $sigma"
+if [ -z $1 ]
+then
+    echo "This program requires an input for normalization"
+    exit
+fi
+if [ $1 -lt 1 ]
+then
+    echo "This program requires an input grater than 1 for normalization"
+    exit
+fi
+
+
+#[0-9]* for zero or more numbers; [0-9][0-9]* one or more; etc. those are patterns with 2 and 3 element resp. \1 \2 \3
+# % echo "123 abc" | sed 's/[0-9]*/& &/'
+# 123 123 abc
+# sed y is like tr

95157cb0c94b6ae472cb7a7bb448953721beebc8
Leonardo Pieripolli <leonardo.pieripolli@studenti.unipd.it>
2025-01-21 13:27:22 +0100

    [solutions] ex3

--- Ex1.sh
+++ Ex1.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-cd $HOME
-rm students/*
-rm -d students/
+cd $HOME #change working directory to $HOME
+rm students/* #delete all elements int students/ if already present elements
+rm -d students/ #delete the directory students, -d is needed
 mkdir -p students #makes dir if not existent
-if [ ! -f "./students/LCP_22-23_students.csv" ] #if not already present 
+if [ ! -f "./students/LCP_22-23_students.csv" ] #if not already present, as in if NOT -find PATH
 then
     wget -v --tries=1 https://www.dropbox.com/s/867rtx3az6e9gm8/LCP_22-23_students.csv --directory-prefix="./students" #import file in ./students
-fi
-cd students
+fi # if is composed of if, then, fi
+cd students # pass to students directory
 touch LCP_22-23_PoD_students.csv #create if not existing
-touch LCP_22-23_Physics_students.csv
-grep "PoD" LCP_22-23_students.csv > LCP_22-23_PoD_students.csv #copy only lines including PoD
-grep "Physics" LCP_22-23_students.csv > LCP_22-23_Physics_students.csv
-max=0
-max_L='A'
-for i in {A..Z}
+touch LCP_22-23_Physics_students.csv #create if not existing
+grep "PoD" LCP_22-23_students.csv > LCP_22-23_PoD_students.csv #copy only lines including PoD from file A to file B
+grep "Physics" LCP_22-23_students.csv > LCP_22-23_Physics_students.csv #copy only lines including Physics from file A to file B
+max=0 #setting a local variable (lowercase) to search maximum
+max_L='A' #setting a local variable (lowercase) to search maximum corresponding letter
+for i in {A..Z} #i assumes values in the alphabet capital letters23_students
 do
-    j=`grep -v -e  "^Family" LCP_22-23_students.csv | grep -c "^$i" LCP_22-23_students.csv` # first gives lines without Family Name, second counts starting with A
-    echo "$i : $j"
-    if [ $j -gt $max ] #compare values, not string
+    # to j is assgined the return of the previous computed part inside `, the left part is executed then passed as input to the right part
+    j=`grep -v -e  "^Family" LCP_22-23_students.csv | grep -c "^$i" LCP_22-23_students.csv` # first gives lines without Family Name (-v exludes -e is followed by the relative element), second counts starting with A
+    echo "$i : $j" #print out both variable; the $ is required in order to obtain the stored value, but works only inside double apix ""
+    if [ $j -gt $max ] #compare values, not string (> is for strings in bash, -gt is grater than for numbers)
     then
-        max=$j
-        max_L=$i
+        max=$j #updating maximum
+        max_L=$i #updating best letter
     fi
-done
-echo "Max surname starting letter $max_L with $max entries"
-lines=`grep '' -c LCP_22-23_students.csv`
-i=2
-while [ $i -le $lines ]
+done #sintax for for is for do done
+echo "Max surname starting letter $max_L with $max entries" # print found output
+lines=`grep '' -c LCP_22-23_students.csv` #counts all lines in file
+i=2 #starting from the third line
+while [ $i -le $lines ] #for each line, it will print it inside a different file, according to the modulo 18
 do
     let g=($i-1)%18 #real computation for variables needs let or (( ))
-    file="Group$g.csv"
-    touch $file
+    file="Group$g.csv" #naming scheme that uses value of i, there are ""
+    touch $file #create file
     awk NR==$i LCP_22-23_students.csv >> $file #selected line with awk
     let i+=1
 done
--- Ex2.sh
+++ Ex2.sh
@@ -1,49 +1,53 @@
 #!/bin/bash
 
-grep -v '^#' data.csv | sed -e 's/,//g' > data.txt
-even=0
-for i in `cat data.txt`
+grep -v '^#' data.csv | sed -e 's/,//g' > data.txt #take the lines in data.csv that don't start with #, substitute (sed) element (-e) from , to nothing
+even=0 #starting to count even
+for i in `cat data.txt` #for element in the dump of the file, so the single number can be selected
 do
-    if [ $i%2 ] #%2 gives 0 if even, that correspond to true in bash
+    #IN BASH TRUE IS 0 AND 1 IS FALSE, [ ] make it interpret as bool, but (( )) is for numbers,  then converted to bools that way
+    if (( $i%2 )) #%2 gives 0 if even, that correspond to true in bash
     then
-        let even+=1
+        let even+=1 #update counter, let is mandatory
     fi
 done
 echo "even numbers = $even"
+#setting new local variable to count
 m=0
 l=0
+ #getting a set value for sigma, $ pass the value instead of the command, |bc make it compute in floating point operation, '' are ok because there are no variables
 sigma=$( echo 'scale=6;100*sqrt(3)/2.0' | bc)
-FILE="data.txt"
-lines=`grep '' -c $FILE`
-i=1
-while [ $i -le $lines ]
+FILE="data.txt" #global variable FILE
+lines=`grep '' -c $FILE` #counts all lines in file
+i=1 #starts from second line
+while [ $i -le $lines ] #cycle over number of lines
 do
-    line=`awk NR==$i $FILE`
-    IFS=' ' read -r X Y Z x y z <<< "$line"
-    d=$( echo "scale=6;sqrt($X*$X+$Y*$Y+$Z*$Z)" | bc)
+    line=`awk NR==$i $FILE` #line is the string of the single i line in FILE
+    IFS=' ' read -r X Y Z x y z <<< "$line" #set variables X, Y, Z, x, y, z from the line using separator ' ', read requires <<<
+    d=$( echo "scale=6;sqrt($X*$X+$Y*$Y+$Z*$Z)" | bc) # assign to d the distance computed
     #echo $d
-    if [ `echo "$d < $sigma" | bc` -eq 0 ]
+    if [ `echo "$d < $sigma" | bc` -eq 0 ] #this is a bool op. because of -eq, check if d<=sigma, using bc to do calculation
     then
-        let m++
+        let m++ #update more than
     else
-        let l++
+        let l++ #update less than
     fi
-    let i++
+    let i++ #update i
 done
 echo "there are $m of distance grater than $sigma"
 echo "there are $l of distance smaller than $sigma"
-if [ -z $1 ]
+if [ -z $1 ] #1 is the first input when calling the script, -z check if it is a NULL, apparently -z is bool op
 then
     echo "This program requires an input for normalization"
     exit
 fi
-if [ $1 -lt 1 ]
+if [ $1 -lt 1 ] #check the normalization if <= 1
 then
     echo "This program requires an input grater than 1 for normalization"
     exit
 fi
-for (( i=1; i<=$1; i++ ))
+for (( i=1; i<=$1; i++ )) #computing operation (())
 do
+    # DIFFERENCE -v for awk is value, for grep is "without"
     #-v passes i as a variable to awk; cycle over NF Number of Fields; $j content of j field, 
     #check if field equal to number and end ($); then print as float j/i
     awk -v i="$i" '{for(j=1;j<=NF;j++) if($j~/^[0-9]+$/) $j=sprintf("%.1f",$j/i)}1' data.txt > "data$i.csv"
